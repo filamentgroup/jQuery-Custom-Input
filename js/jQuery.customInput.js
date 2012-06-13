75775d7cd7082ba9f70ef0fcbd59ed156fb2b4e0
George T. Walters II <walterg2@gmail.com>
2012-06-13 11:26:37 -0400

    Updates to plug in to add in a span tag to hold the sprite image. Also added needed libs to the project for example page.

--- js/jQuery.customInput.js
+++ js/jQuery.customInput.js
@@ -1,3 +1,4 @@
+/*globals jQuery, $ */
 /**
  * --------------------------------------------------------------------
  * jQuery customInput plugin
@@ -6,40 +7,38 @@
  * licensed under MIT (filamentgroup.com/examples/mit-license.txt)
  * --------------------------------------------------------------------
  */
-jQuery.fn.customInput = function(){
-	return $(this).each(function(){	
-		if($(this).is('[type=checkbox],[type=radio]')){
-			var input = $(this);
-			
+jQuery.fn.customInput = function () {
+	"use strict";
+	return $(this).each(function () {
+		if ($(this).is('[type=checkbox],[type=radio]')) {
+			var input = $(this),
 			// get the associated label using the input's id
-			var label = $('label[for="'+input.attr('id')+'"]');
-			
+				label = $('label[for="' + input.attr('id') + '"]');
+
+			// add in an empty span tag to contain the sprite image
+			label.prepend("<span></span");
+
 			// wrap the input + label in a div 
-			input.add(label).wrapAll('<div class="custom-'+ input.attr('type') +'"></div>');
-			
+			input.add(label).wrapAll('<div class="custom-' + input.attr('type') + '"></div>');
+
 			// necessary for browsers that don't support the :hover pseudo class on labels
 			label.hover(
-				function(){ $(this).addClass('hover'); },
-				function(){ $(this).removeClass('hover'); }
+				function () { $(this).addClass('hover'); },
+				function () { $(this).removeClass('hover'); }
 			);
-			
+
 			//bind custom event, trigger it, bind click,focus,blur events					
-			input.bind('updateState', function(){	
-				input.is(':checked') ? label.addClass('checked') : label.removeClass('checked checkedHover checkedFocus'); 
+			input.bind('updateState', function () {
+				if (input.is(':checked')) {
+					label.addClass('checked');
+				} else {
+					label.removeClass('checked');
+				}
 			})
-			.trigger('updateState')
-			.click(function(){ 
-				$('input[name="'+ $(this).attr('name') +'"]').trigger('updateState'); 
-			})
-			.focus(function(){ 
-				label.addClass('focus'); 
-				if(input.is(':checked')){  $(this).addClass('checkedFocus'); } 
-			})
-			.blur(function(){ label.removeClass('focus checkedFocus'); });
+				.trigger('updateState')
+				.click(function () { $('input[name="' + $(this).attr('name') + '"]').trigger('updateState'); })
+				.focus(function () { label.addClass('focus'); })
+				.blur(function () { label.removeClass('focus'); });
 		}
 	});
 };
-
-
-	
-	
